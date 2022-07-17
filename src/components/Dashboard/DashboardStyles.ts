fc8751a66e40976ffa0a05d1dc4bdfa6cbcca95a
manchikooo <manchiko.man31@mail.ru>
2022-07-17 10:43:55 +0300

    made mobile layout

--- src/components/Dashboard/DashboardStyles.ts
+++ src/components/Dashboard/DashboardStyles.ts
@@ -9,4 +9,8 @@ export const DashboardBlock = styled.div`
   padding: 0 10px;
   display: flex;
   justify-content: space-between;
+  @media (max-width: 530px) {
+    flex-direction: column;
+    padding: 50px 10px;
+  }
 `;
